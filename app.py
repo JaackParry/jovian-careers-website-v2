017c2357625dad280b41b2b43383db861ae147e5
JaackParry <51380478+JaackParry@users.noreply.github.com>
2023-04-30 20:56:59 +0000

    creating individual job pages, updating bootstrap files, dynamic pages now working

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
 
@@ -30,17 +30,29 @@ app = Flask(__name__)
   }
 ]'''
 
+
 @app.route("/")
 def hello_jovian():
   jobs = load_jobs_from_db()
-  return render_template('home.html', 
-                         jobs=jobs, #this is saying that wherever jobs appears in home.html to use our jobs variable
-                         company_name="Jack's")
+  return render_template(
+    'home.html',
+    jobs=jobs)  #this is saying that wherever jobs appears in home.html to use our jobs variable
+
 
 @app.route("/api/jobs")
 def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
+
+
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+  if not job:
+    return "Not Found", 404 
+    #this line and the one above tells the code to return not found and a 404 error if a job id is listed that doesn't exist. This stops the static html rendering and having a weird looking webpage
+  return render_template('jobpage.html', job=job)
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -4,16 +4,17 @@ import os
 
 #using our secrets manager to add the DB connection string, it's very bad practice to be storing plain text information in our code
 
-db_connection_string=os.environ['DB_CONNECTION_STRING']
+db_connection_string = os.environ['DB_CONNECTION_STRING']
 
 #first we have to create an engine to connect to the db - then we will be able to extract data from the db
-engine = create_engine(db_connection_string, connect_args={
-  #you need to use ssl to connect to mysql as it won't allow insecure connections, you'll get an error otherwise
-  "ssl": {
-    "ssl_ca":"/etc/ssl/ca.pem"
-  }
-}
-)
+engine = create_engine(
+  db_connection_string,
+  connect_args={
+    #you need to use ssl to connect to mysql as it won't allow insecure connections, you'll get an error otherwise
+    "ssl": {
+      "ssl_ca": "/etc/ssl/ca.pem"
+    }
+  })
 
 #this allows us to use the connection we defined above to execute against the db. once the sequence has run then the connection will drop
 with engine.connect() as conn:
@@ -30,17 +31,29 @@ def load_jobs_from_db():
       jobs.append(row._asdict())
     return jobs
 
-  
-  
+
+#this is a slight different function that will only return one particular job depending on it's id. if an ID is specified that doesn't exist in the DB then we will just return 0
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"),
+                          {'val': id})
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    else:
+      row = rows[0]
+      column_names = list(result.keys())
+      return {column_names[i]: row[i] for i in range(len(column_names))}
+
+
 #now we're creating a dictionary withe the rows from the db. the rows were not iterable so we needed to use the _asdict function to convert
-  
   '''result_dicts = []
   for row in result.all():
     result_dicts.append(row._asdict())
 
   print(result_dicts)'''
-    
- #code below is testing out pulling different results 
+
+  #code below is testing out pulling different results
   '''print("type(result)", type(result))
   result_all = result.all()
   print("type(result.all)", type(result_all))
