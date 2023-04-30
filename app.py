24e04f88c6b3f9ecdf70b43d18872dc41a111274
JaackParry <51380478+JaackParry@users.noreply.github.com>
2023-04-30 19:17:55 +0000

    added connection to db, dynamic data now showing in the webpage

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, jsonify
+from database import load_jobs_from_db
 
 app = Flask(__name__)
 
-#creating an array to use as dynamic content on the web page
-JOBS = [
+#creating an array to use as dynamic content on the web page - we have since replaced this with our database information
+'''JOBS = [
   {
     'id': 1,
     'title': 'Data Analyst',
@@ -27,15 +28,19 @@ JOBS = [
     'location': 'San Francisco, USA',
     'salary': '$150,000'
   }
-]
+]'''
 
 @app.route("/")
-def hello_world():
-  return render_template('home.html', jobs=JOBS,company_name="Jack's")
+def hello_jovian():
+  jobs = load_jobs_from_db()
+  return render_template('home.html', 
+                         jobs=jobs, #this is saying that wherever jobs appears in home.html to use our jobs variable
+                         company_name="Jack's")
 
 @app.route("/api/jobs")
 def list_jobs():
-  return jsonify(JOBS)
+  jobs = load_jobs_from_db()
+  return jsonify(jobs)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
---
+++ database.py
@@ -0,0 +1,52 @@
+#packages required to connect to the db
+from sqlalchemy import create_engine, text
+import os
+
+#using our secrets manager to add the DB connection string, it's very bad practice to be storing plain text information in our code
+
+db_connection_string=os.environ['DB_CONNECTION_STRING']
+
+#first we have to create an engine to connect to the db - then we will be able to extract data from the db
+engine = create_engine(db_connection_string, connect_args={
+  #you need to use ssl to connect to mysql as it won't allow insecure connections, you'll get an error otherwise
+  "ssl": {
+    "ssl_ca":"/etc/ssl/ca.pem"
+  }
+}
+)
+
+#this allows us to use the connection we defined above to execute against the db. once the sequence has run then the connection will drop
+with engine.connect() as conn:
+  #running a query to just pull all data from the jobs table
+  result = conn.execute(text("select * from jobs"))
+
+
+#here we are pulling our list of jobs from the db and into a dictionary. this is all based upon the work that is now commented out in database.py. it is good practice to keep all our database-related functionality in a database file rather than mixing it across the app.py file
+def load_jobs_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))
+    jobs = []
+    for row in result.all():
+      jobs.append(row._asdict())
+    return jobs
+
+  
+  
+#now we're creating a dictionary withe the rows from the db. the rows were not iterable so we needed to use the _asdict function to convert
+  
+  '''result_dicts = []
+  for row in result.all():
+    result_dicts.append(row._asdict())
+
+  print(result_dicts)'''
+    
+ #code below is testing out pulling different results 
+  '''print("type(result)", type(result))
+  result_all = result.all()
+  print("type(result.all)", type(result_all))
+  first_result = result_all[0]
+  print("type(first_result):",type(first_result))
+  first_result_dict = dict([result_all])
+  print(type(first_result_dict))
+  print(first_result_dict)
+'''
